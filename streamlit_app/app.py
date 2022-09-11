277d041f7b6689cb6f8ef00a3630013a0b59bad8
Nebil Arega <36981187+nebilarega@users.noreply.github.com>
2022-09-12 01:56:48 +0300

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,1 +1,74 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+import home
+# import data_visulization
+import model
+# from config import nav_image
+
+# Pages in the app
+PAGES = {
+    "Home": home,
+    # "Data Visualization": data_visulization,
+    "Data Prediction": model
+}
+
+# background styling
+page_bg = '''
+<style>
+body {
+background-color : #f4f4f4;
+}
+</style>
+'''
+st.markdown(page_bg, unsafe_allow_html=True)
+
+# navbaar styling
+st.markdown(
+    """
+<style>
+.sidebar .sidebar-content {
+    background-image: linear-gradient(#292929,#E65142;9);
+    color: black;
+    align-text: center;
+}
+hr.rounded {
+        border-top: 6px solid #E65142;
+        border-radius: 5px;
+    }
+</style>
+""", unsafe_allow_html=True,
+)
+
+# inseting image in the sidebar
+nav_image = plt.imread('images/10aclogo.png')
+st.sidebar.image(nav_image, use_column_width=True)
+
+# navbar content-1
+html3 = '''
+<h2 style="text-align: center;">Sales Prediction</h2>
+<p style="text-align: center; font-size: 15px">This project is part of 10Academy week3 project -
+ sales prediction using Rossmann data
+<hr class="rounded">
+'''
+st.sidebar.markdown(html3, unsafe_allow_html=True)
+
+st.sidebar.title("Explore")
+
+# radio selection for the pages
+selection = st.sidebar.radio("", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()
+
+# navbar content-2
+html4 = '''
+<br>
+<br>
+<p><b>Data Visualization -</b> Analyze the sales data accross various quarters of financial year 2014 for <i>Rossmann Stores</i> using 
+multiple charts-box, line, bar, time series etc.</p>
+<br>
+<p><b>Data Prediction -</b> Predict the future sales set to different features or columns. Predictions can be made using default entries
+or select one or multiple fields to be manually edited for predictions.</p>
+'''
+st.sidebar.markdown(html4, unsafe_allow_html=True)
 
